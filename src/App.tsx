87ed68c77c9a0ab3c4bf0251dd5652d42c897806
candartozan <candartozan@gmail.com>
2025-01-17 21:08:37 +0300

    splash, icon and app name changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from '@navigation/Navigation';
 import '../global.css';
 import {StatusBar} from 'react-native';
 import {store} from '@redux/store';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#001427" />
